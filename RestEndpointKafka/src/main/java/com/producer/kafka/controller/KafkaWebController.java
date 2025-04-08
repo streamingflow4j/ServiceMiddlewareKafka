b9a6572f18999ac214f52161d93c3b2211ba20fa
DESKTOP-J7F8RBI\Usuario <herberttb@gmail.com>
2025-04-08 10:51:46 -0300

    properties ajustments

--- RestEndpointKafka/src/main/java/com/producer/kafka/controller/KafkaWebController.java
+++ RestEndpointKafka/src/main/java/com/producer/kafka/controller/KafkaWebController.java
@@ -3,13 +3,7 @@ package com.producer.kafka.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.producer.kafka.model.Entity;
@@ -18,14 +12,19 @@ import com.producer.kafka.service.KafkaSender;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RequestMapping(value = "/kafka")
 @Log4j2
 public class KafkaWebController {
 
-	@Autowired
-	KafkaSender kafkaSender;
-	
-	@ResponseStatus(HttpStatus.OK)
+
+	private final KafkaSender kafkaSender;
+
+    public KafkaWebController(KafkaSender kafkaSender) {
+        this.kafkaSender = kafkaSender;
+    }
+
+    @ResponseStatus(HttpStatus.OK)
 	@PostMapping(value = "/data/create")
 	public String newData(@RequestBody Entity entity) throws JsonProcessingException {	
 		kafkaSender.sendData(entity);
